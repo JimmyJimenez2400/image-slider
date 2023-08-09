f17509ba5260c6eb8035c9b853077d9bb47bbfbe
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-08-09 05:38:59 -0400

    remove function hello and call pageLoad function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-import _ from "lodash";
+import pageLoad from "./pageLoad./pageLoad";
 import "./style.css";
 
-function hello() {
-  const sectionContainer = document.createElement("div");
-
-  const h1 = document.createElement("h1");
-  h1.innerHTML = _.join(["Hello", "webpack"], " ");
-  h1.classList.add("hello");
-
-  const h2 = document.createElement("h2");
-  h2.textContent = "HELLLLO WORLD";
-
-  sectionContainer.appendChild(h1);
-  sectionContainer.appendChild(h2);
-
-  return sectionContainer;
-}
-
-document.body.appendChild(hello());
+pageLoad();
