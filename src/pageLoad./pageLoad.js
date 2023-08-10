7734c2bcbcb6386950ed0333cc5930dded8e8892
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-08-10 16:44:34 -0400

    import images to add to imageCarouseMaker argument

--- src/pageLoad./pageLoad.js
+++ src/pageLoad./pageLoad.js
@@ -1,9 +1,13 @@
 import imageCarouselMaker from "../imageCarousel/imageCarouselMaker";
 
+import luffy from "../pictures/luffy_statue.jpg";
+import gundam from "../pictures/gundamn_statue.jpg";
+import liberty from "../pictures/liberty_statue.jpg";
+
 export default function pageLoad() {
   const content = document.querySelector("#content");
 
-  const testArray = ["luffy", "gundam", "liberty"];
+  const testArray = [luffy, gundam, liberty];
 
   content.appendChild(imageCarouselMaker(testArray));
 
